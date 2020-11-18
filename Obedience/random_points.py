4ba060733b5c74d34c274b86d138fe17b87a2f5a
rutazzz <zalyte.ruta@gmail.com>
2020-11-18 18:32:40 +0200

    Started generateIntermediates

--- Obedience/random_points.py
+++ Obedience/random_points.py
@@ -3,13 +3,14 @@ import random
 
 
 points = []
+intrm = []
 
 # Define area parameters:
 xMax = 5
 yMax = 5
 
 # Define number of turns:
-xTurns = 4
+xTurns = 2
 
 # Define start and finish:
 start = numpy.array([1, 1])
@@ -30,6 +31,22 @@ def generateTurns(xTurns):
 
 generateTurns(xTurns)
 points.append(finish)
-print(points)
+
+for i in points:
+    print("points", i)
 
 # Generate intermediate turns
+
+
+def generateIntermediates(xTurns):
+    i = 0
+    while i <= xTurns:
+        x = numpy.array([points[i][0], points[i + 1][1]])
+        print("x", x)
+        intrm.append(x)
+        i += 1
+
+
+for i in intrm:
+    print(i)
+generateIntermediates(xTurns)
